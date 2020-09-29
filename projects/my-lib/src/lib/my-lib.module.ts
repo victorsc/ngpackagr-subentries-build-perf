7cd7befa36ab37a595da867ed158f6c456026b69
Victor Scaiceanu <victor.scaiceanu@amadeus.com>
2020-09-29 21:42:54 +0200

    add modules

--- projects/my-lib/src/lib/my-lib.module.ts
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -1,12 +1,111 @@
 import { NgModule } from '@angular/core';
 import { MyLibComponent } from './my-lib.component';
+import { Module1Module } from './module1/module1.module';
+import { Module2Module } from './module2/module2.module';
+import { Module3Module } from './module3/module3.module';
+import { Module4Module } from './module4/module4.module';
+import { Module5Module } from './module5/module5.module';
+import { Module6Module } from './module6/module6.module';
+import { Module7Module } from './module7/module7.module';
+import { Module8Module } from './module8/module8.module';
+import { Module9Module } from './module9/module9.module';
+import { Module10Module } from './module10/module10.module';
+import { Module11Module } from './module11/module11.module';
+import { Module12Module } from './module12/module12.module';
+import { Module13Module } from './module13/module13.module';
+import { Module14Module } from './module14/module14.module';
+import { Module15Module } from './module15/module15.module';
+import { Module16Module } from './module16/module16.module';
+import { Module17Module } from './module17/module17.module';
+import { Module18Module } from './module18/module18.module';
+import { Module19Module } from './module19/module19.module';
+import { Module20Module } from './module20/module20.module';
+import { Module21Module } from './module21/module21.module';
+import { Module22Module } from './module22/module22.module';
+import { Module23Module } from './module23/module23.module';
+import { Module24Module } from './module24/module24.module';
+import { Module25Module } from './module25/module25.module';
+import { Module26Module } from './module26/module26.module';
+import { Module27Module } from './module27/module27.module';
+import { Module28Module } from './module28/module28.module';
+import { Module29Module } from './module29/module29.module';
+import { Module30Module } from './module30/module30.module';
+import { Module31Module } from './module31/module31.module';
+import { Module32Module } from './module32/module32.module';
+import { Module33Module } from './module33/module33.module';
+import { Module34Module } from './module34/module34.module';
+import { Module35Module } from './module35/module35.module';
+import { Module36Module } from './module36/module36.module';
+import { Module37Module } from './module37/module37.module';
+import { Module38Module } from './module38/module38.module';
+import { Module39Module } from './module39/module39.module';
+import { Module40Module } from './module40/module40.module';
+import { Module41Module } from './module41/module41.module';
+import { Module42Module } from './module42/module42.module';
+import { Module43Module } from './module43/module43.module';
+import { Module44Module } from './module44/module44.module';
+import { Module45Module } from './module45/module45.module';
+import { Module46Module } from './module46/module46.module';
+import { Module47Module } from './module47/module47.module';
+import { Module48Module } from './module48/module48.module';
+import { Module49Module } from './module49/module49.module';
+import { Module50Module } from './module50/module50.module';
 
 
 
 @NgModule({
   declarations: [MyLibComponent],
   imports: [
-  ],
+  Module1Module,
+  Module2Module,
+  Module3Module,
+  Module4Module,
+  Module5Module,
+  Module6Module,
+  Module7Module,
+  Module8Module,
+  Module9Module,
+  Module10Module,
+  Module11Module,
+  Module12Module,
+  Module13Module,
+  Module14Module,
+  Module15Module,
+  Module16Module,
+  Module17Module,
+  Module18Module,
+  Module19Module,
+  Module20Module,
+  Module21Module,
+  Module22Module,
+  Module23Module,
+  Module24Module,
+  Module25Module,
+  Module26Module,
+  Module27Module,
+  Module28Module,
+  Module29Module,
+  Module30Module,
+  Module31Module,
+  Module32Module,
+  Module33Module,
+  Module34Module,
+  Module35Module,
+  Module36Module,
+  Module37Module,
+  Module38Module,
+  Module39Module,
+  Module40Module,
+  Module41Module,
+  Module42Module,
+  Module43Module,
+  Module44Module,
+  Module45Module,
+  Module46Module,
+  Module47Module,
+  Module48Module,
+  Module49Module,
+  Module50Module],
   exports: [MyLibComponent]
 })
 export class MyLibModule { }
